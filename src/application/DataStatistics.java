851ce65cce593f0e170028c34441bff417bbb0f1
Mary Byrne <maryebyrne2@gmail.com>
2025-11-22 16:55:33 +0000

    Updated DataStatistics and GUI updates

--- src/application/DataStatistics.java
+++ src/application/DataStatistics.java
@@ -4,6 +4,9 @@
 *  This class is to calculate and display summary statistics about the data in the file, such as number
 *  of rows, averages and totals as relevant to the dataset.
 *  
+*  //average temp and humidity
+*  //total rainy,windy etc days
+*  
 *  Author: Mary Byrne
 *  
 *  2025
@@ -13,6 +16,138 @@
 
 package application;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class DataStatistics {
+	
+	//method to calc average temperatures 
+	public static double calcAverageTemp(String[][] data) {
+		
+	    double sum = 0.0;
+	    int count = 0;
+	    
+	    //temperature column index
+	    int tempCol = 2;
+
+	    //loop through rows 
+	    for (String[] row : data) {
+
+	    	//check column (containing temps) exists and isn't empty
+	        if (row.length > tempCol && row[tempCol] != null && !row[tempCol].isEmpty()) {
+	            try {
+	            	//convert temps from txt file from string to double
+	                double temp = Double.parseDouble(row[tempCol]);
+	                //add to total
+	                sum += temp;
+	                //increase count
+	                count++;
+	                
+	            } catch (NumberFormatException e) {
+	                //ignore invalid values
+	            }
+	        }
+	    }
+
+	    //error checking if count = 0 don't complete equation
+	    if (count == 0) return 0;
+
+	    //calc average
+	    double avg = sum / count;
+
+	    //round to 1 decimal point
+	    BigDecimal rounded = new BigDecimal(avg);
+	    rounded = rounded.setScale(1, RoundingMode.HALF_UP);
+
+	    //return final value (rounded to 1 decimal point)
+	    return rounded.doubleValue();
+	}
+
+	//method to calc average humidity percentage 
+	public static double calcAverageHumid(String[][] data) {
+
+	    double sum = 0.0;
+	    int count = 0;
+	    
+	    //humditiy column index
+	    int humidityCol = 3;
+
+	    //loop through rows
+	    for (String[] row : data) {
+
+	    	//check column (containing humidity %) exists and isn't empty
+	        if (row.length > humidityCol && row[humidityCol] != null && !row[humidityCol].isEmpty()) {
+
+	            try {
+	            	//convert humidity % from txt file from string to double
+	                double humid = Double.parseDouble(row[humidityCol]);
+	                sum += humid;
+	                count++;
+
+	            } catch (NumberFormatException e) {
+	                //ignore invalid values
+	            }
+	        }
+	    }
+
+	    //error checking if count = 0 don't complete equation
+	    if (count == 0) return 0;
+
+	    //calc average humidity %
+	    double avg = sum / count;
+
+	    // Round to 1 decimal place
+	    BigDecimal result = new BigDecimal(avg);
+	    result = result.setScale(1, RoundingMode.HALF_UP);
+
+	    //return final value (rounded to 1 decimal point)
+	    return result.doubleValue();
+	}
+
+	//method to calc total of each weather condition
+	public static String calcTotalConditions(String[][] data) {
+
+	    int sunnyCount = 0;
+	    int cloudyCount = 0;
+	    int rainCount = 0;
+	    int snowCount = 0;
+	    
+	    //condition column index
+	    int conditionCol = 6;
+
+	    //loop through rows
+	    for (String[] row : data) {
+	    	
+	    	//check the Conditions column exists
+	        if (row.length > conditionCol && row[conditionCol] != null) {
+
+	        	//remove any leading/trailing spaces
+	            String cond = row[conditionCol].trim();
+
+	            //switch statement to increment counts
+	            switch (cond) {
+	                case "Sunny":
+	                    sunnyCount++;
+	                    break;
+	                case "Cloudy":
+	                    cloudyCount++;
+	                    break;
+	                case "Rain":
+	                    rainCount++;
+	                    break;
+	                case "Snow":
+	                    snowCount++;
+	                    break;
+	            }
+	        }
+	    }
+
+	    //return amounts of each condition
+	    return "Sunny days: " + sunnyCount + "\n" +
+	           "Cloudy days: " + cloudyCount + "\n" +
+	           "Rain days: " + rainCount + "\n" +
+	           "Snow days: " + snowCount;
+	}
+
 
 }
--- src/application/DatasetExplorerGUI.java
+++ src/application/DatasetExplorerGUI.java
@@ -17,63 +17,77 @@ package application;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("serial")
 public class DatasetExplorerGUI extends JFrame implements ActionListener{
+		
+			JFrame frame = new JFrame("Dataset table");
+			JPanel panel1 = new JPanel();
+			JButton button1, button2, button3;
+			JTextField t1;
+			
+			//column array
+			String[] columns= {
+					"Station_ID","Date","Temp_C",
+					"Humidity_%","Wind_kph","Precip_mm","Condition"
+					};
+			
+			//loading data from dataset into 2d array
+			String[][] tableData = FileProcessor.loadDataset("dataset.txt");
+			
+			//create table using columns and data
+	        JTable datasetTable = new JTable(tableData, columns);
+			
+	        public DatasetExplorerGUI() {
+
+	        	//LEFT_ALIGNMENT
+	            panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+	            frame.add(panel1);
+
+	            //create the default table model
+	            DefaultTableModel model = new DefaultTableModel(tableData, columns);
+	            JTable datasetTable = new JTable(model);
+
+	            //add row button
+	            button1 = new JButton("Add empty row");
+
+	            //action listener for add row button
+	            button1.addActionListener(e -> {
+	            	
+	                System.out.println("'Row added");
+	                model.addRow(new String[columns.length]);
+	                
+	            });
+
+	            panel1.add(button1);
+	            panel1.add(new JScrollPane(datasetTable));
+	            
+	            System.out.println("Weather Forecast Statistics:");
+	            double averageTemp = DataStatistics.calcAverageTemp(tableData);
+	            System.out.println("The average temperature is " + averageTemp);
+	            
+	            double averageHumid = DataStatistics.calcAverageHumid(tableData);
+	            System.out.println("The average humidity percentage is " + averageHumid);
+	            
+	            String totalConditions = DataStatistics.calcTotalConditions(tableData);
+	            System.out.println(totalConditions);
+
+	            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	            frame.pack();
+	            frame.setVisible(true);
+	        }
+
 	
-	public DatasetExplorerGUI(){
-		
-		//creating the window and panel to hold table
-		JFrame frame = new JFrame("Dataset table");
-		JPanel panel1 = new JPanel();
-		
-		//add vertical layout
-		panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
-		
-		//add name to table
-		JLabel staticTableName = new JLabel("Weather Forcast Table");
-		
-		//adding panel with label to frame 
-		frame.add(panel1);
-		panel1.add(staticTableName);
-		
-		//column titles
-		String[] columns= {
-				"Station_ID","Date","Temp_C",
-				"Humidity_%","Wind_kph","Precip_mm","Condition"
-				};
-		
-		//loading data from dataset into 2d array
-		String[][] tableData = FileProcessor.loadDataset("dataset.txt");
-
-		//create table using columns and data
-        JTable datasetTable = new JTable(tableData, columns);
-        
-        //make table scrollable
-        panel1.add(new JScrollPane(datasetTable));
-
-        //close program when window is closed
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        //resize window to fit componants
-        frame.pack();
-        
-        //make window visable
-        frame.setVisible(true);
-	}
+	//main method
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new DatasetExplorerGUI());
+    }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	//main method
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new DatasetExplorerGUI());
-    }
     
 }
